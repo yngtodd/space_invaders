d49719d31b77486a3d56d4c444790287ac035114
yngtodd <young.todd.mk@gmail.com>
2018-10-06 13:26:22 -0400

    Refactor key events

--- space_invaders/mechanics/game_functions.py
+++ space_invaders/mechanics/game_functions.py
@@ -7,18 +7,26 @@ def check_events(ship):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = True
-            if event.key == pygame.K_LEFT:
-                ship.moving_left = True
-
+            check_keydown_events(event, ship)
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = False
-            if event.key == pygame.K_LEFT:
-                ship.moving_left = False
+            check_keyup_events(event, ship)
+
+
+def check_keydown_events(event, ship):
+    """Respond to keypresses."""
+    if event.key == pygame.K_RIGHT:
+         ship.moving_right = True
+    if event.key == pygame.K_LEFT:
+         ship.moving_left = True
+
+
+def check_keyup_events(event, ship):
+    """Respond to key releases."""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    if event.key == pygame.K_LEFT:
+        ship.moving_left = False
 
 
 def update_screen(screen_settings, screen, ship):
