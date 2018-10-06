fcf8595eca4307e67ee3de89c8280995ef84b005
yngtodd <young.todd.mk@gmail.com>
2018-10-06 11:04:50 -0400

    Add ship

--- space_invaders/alien_invasion.py
+++ space_invaders/alien_invasion.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 
+from space_invaders.sprites import Ship
 from space_invaders.settings import ScreenSettings
 
 
@@ -10,12 +11,15 @@ def run_game():
     screen = pygame.display.set_mode(screen_settings.width, screen_settings.height)
     pygame.display.set_caption("Space Invaders")
 
+    ship = Ship(screen)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
 
         screen.fill(screen_settings.bg_color)
+        ship.blitme()
         pygame.display.flip()
 
 
