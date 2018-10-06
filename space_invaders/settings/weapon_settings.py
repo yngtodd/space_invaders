f070a28c617ecb924578fa7d23f9bb88b772be0a
yngtodd <young.todd.mk@gmail.com>
2018-10-06 17:58:12 -0400

    Updating bullet color

--- space_invaders/settings/weapon_settings.py
+++ space_invaders/settings/weapon_settings.py
@@ -3,7 +3,7 @@ class WeaponSettings:
     Stores values for ship weapons.
     """
     def __init__(self):
-        self.speed_factor = 2.5
+        self.speed_factor = 3
         self.width = 3
         self.height = 15
-        self.color = (60, 60, 60)
+        self.color = (90, 30, 60)
