62a4f278afe861881b9769043c90b9f209b00805
yngtodd <young.todd.mk@gmail.com>
2018-10-06 17:29:56 -0400

    Giving the ship a weapon

---
+++ space_invaders/settings/weapon_settings.py
@@ -0,0 +1,9 @@
+class WeaponSettings:
+    """
+    Stores values for ship weapons.
+    """
+    def __init__(self):
+        self.speed_factor = 2.5
+        self.width = 3
+        self.height = 15
+        self.color = (60, 60, 60)
---
+++ space_invaders/sprites/weapon.py
@@ -0,0 +1,17 @@
+import pygame
+from pygame.sprite import Sprite
+
+
+class Weapon(Sprite):
+    """
+    Weapons fired by the ship.
+    """
+    def __init__(self, weapon_settings, screen, ship):
+        super().__init__()
+        self.screen = screen
+        self.rect = pygame.Rect(0, 0, weapon_settings.width, weapon_settings.height)
+        self.rect.centerx = ship.rect.centerx
+        self.rect.top = ship.rect.top
+        self.color = weapon_settings.color
+        self.speed_factor = weapon_settings.speed_factor
+        self.y = float(self.rect.y)
