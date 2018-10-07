af4372320c0e85493cd720cf0e626951fd00ba41
yngtodd <young.todd.mk@gmail.com>
2018-10-07 15:12:52 -0400

    Adding game stats

--- space_invaders/settings/ship_settings.py
+++ space_invaders/settings/ship_settings.py
@@ -2,3 +2,4 @@ class ShipSettings:
     """Stores settings for friendly ship."""
     def __init__(self):
         self.ship_speed_factor = 2.5
+        self.ship_limit = 3
---
+++ space_invaders/stats/game_stats.py
@@ -0,0 +1,10 @@
+class GameStats:
+    """Track statistics for Space Invaders."""
+    def __init__(self, ship_settings, alien_settings):
+        self.ship_settings = ship_settings
+        self.alien_settings = alien_settings
+        self.reset_stats()
+
+    def reset_stats(self):
+        """Initialize statistics for the game."""
+        self.ships_left = self.ship_settings.ship_limit
