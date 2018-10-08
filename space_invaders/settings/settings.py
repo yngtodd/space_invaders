45abec2a0aa0f05fd764024fdf8b01edd1049be7
yngtodd <young.todd.mk@gmail.com>
2018-10-07 23:20:22 -0400

    Scaling score as the game speeds up

--- space_invaders/settings/settings.py
+++ space_invaders/settings/settings.py
@@ -3,7 +3,9 @@ class Settings:
     def __init__(self, speedup_scale=1.1):
         self.speedup_scale = speedup_scale
         self.alien_points = 50
+        self.score_scale = 1.5
 
     def increase_speed(self):
         """Increase speed dynamics through gameplay."""
         self.speed_factor *= self.speedup_scale
+        self.alien_points = int(self.alien_points * self.score_scale)
