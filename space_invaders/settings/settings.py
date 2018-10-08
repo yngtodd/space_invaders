c0a913ad3637dbf7e1985cc3706ef6d286474762
yngtodd <young.todd.mk@gmail.com>
2018-10-07 22:53:42 -0400

    Adding scoreboard

---
+++ space_invaders/settings/settings.py
@@ -0,0 +1,8 @@
+class Settings:
+    """Settings used by all sub-settings."""
+    def __init__(self, speedup_scale=1.1):
+        self.speedup_scale = speedup_scale
+
+    def increase_speed(self):
+        """Increase speed dynamics through gameplay."""
+        self.speed_factor *= self.speedup_scale
---
+++ space_invaders/stats/scoreboard.py
@@ -0,0 +1,26 @@
+import pygame.font
+
+
+class ScoreBoard:
+    """Report scoring information"""
+    def __init__(self, screen_settings, screen, stats):
+        self.screen = screen
+        self.screen_rect = screen.get_rect()
+        self.screen_settings = screen_settings
+        self.stats = stats
+        self.text_color = (30, 30, 30)
+        self.font = pygame.font.SysFont(None, 48)
+        self.prep_score()
+
+    def prep_score(self):
+        """Turn score into a rendered image."""
+        score_str = str(self.stats.score)
+        self.score_img = self.font.render(score_str, True, self.text_color, self.screen_settings.bg_color)
+        self.score_rect = self.score_img.get_rect()
+        self.score_rect.right = self.screen_rect.right - 20
+        self.score_rect.top = 20
+
+    def show_score(self):
+        """Draw score to the screen."""
+        self.screen.blit(self.score_img, self.score_rect)
+
