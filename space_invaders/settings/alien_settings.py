0c9c8187c05aa04ba95d3c05936a4bc85243d1c6
yngtodd <young.todd.mk@gmail.com>
2018-10-07 22:39:47 -0400

    Satisfied with the speed settings:

--- space_invaders/settings/alien_settings.py
+++ space_invaders/settings/alien_settings.py
@@ -5,6 +5,6 @@ class AlienSettings(Settings):
     """Stores settings for alien ship."""
     def __init__(self):
         super().__init__()
-        self.speed_factor = 1.25
+        self.speed_factor = 2.5
         self.fleet_drop_speed = 15
         self.fleet_direction = 1
